4452f1a9e59760be3b141dfea7bcacacbdc5643b
chpill <etienne@spillemaeker.com>
2017-05-19 08:52:23 +0200

    Release 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.chpill.re-frankenstein "0.0.1-SNAPSHOT"
+(defproject com.chpill.re-frankenstein "0.0.1"
   :description  "The deviant fork of re-frame"
   :url          "https://github.com/chpill/re-frankenstein.git"
   :license      {:name "MIT"}
